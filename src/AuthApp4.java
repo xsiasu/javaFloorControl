10c16b043141c7f57a241b5993850ec7f2f11037
sgkim <sgkim@the-51.com>
2022-06-06 22:22:43 +0900

    auth4

--- src/AuthApp4.java
+++ src/AuthApp4.java
@@ -2,6 +2,33 @@ public class AuthApp4 {
     public static void main(String[] args) {
 
 
+        //아이디
+        //passoword
+
+
+        String[][] users = {
+                {"sean","1111"},
+                {"sohui","2222"},
+                {"hoyeong","3333"},
+        };
+
+        String inputId = args[0];
+        String inputPass = args[1];
+        boolean isLoinged = false;
+
+        for (int i =0;users.length > i;i++){
+            String[] current = users[i];
+            if (current[0].equals(inputId) && current[1].equals(inputPass)) {
+                isLoinged = true;
+                break;
+            }
+        }
+
+        if(isLoinged) {
+            System.out.println("success");
+        }else {
+            System.out.println("fail");
+        }
 
     }
 }
